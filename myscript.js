e060dc5ea7d4fb191087bf0376752a1e72ea979c
zeed3v <98282880+zeed3v@users.noreply.github.com>
2022-03-16 16:57:19 -0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+$(window).on('load', function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshowSpeed: 7000,
+        direction: "vertical",
+        reverse: "true",
+        pauseOnHover: true
+    });
+});
